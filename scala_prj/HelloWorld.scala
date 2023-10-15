2cb32c715006d49392ebec3d54082f2d433728df
tarorin0305 <dayutakatarorin@gmail.com>
2023-10-15 22:29:44 +0900

    update

--- scala_prj/HelloWorld.scala
+++ scala_prj/HelloWorld.scala
@@ -1,12 +1,4 @@
 object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-    println(add(1, 2))
-    println(increment(1))
-    println(getFirstCharacter("Hello"))
-    println(wordScore("Hello"))
-  }
-
   def firstTwo(list: List[String]): List[String] = {
     list.slice(0, 2)
   }
@@ -57,15 +49,59 @@ object Main {
     return str.charAt(0)
   }
 
-  def wordScore(word: String): Int = {
-    return word.length()
+  def wordScoreFunc(word: String): Int = {
+    return word.replaceAll("a", "").length
   }
 
-  def score(word: String): Int = {
-    word.replaceAll("a", "").length
+  def rankedWords(wordScore: String => Int, words: List[String]): List[String] = {
+    def negativeScore(word: String): Int = -wordScore(word)
+    words.sortBy(negativeScore)
   }
 
-  val words = List("rust", "java")
+  def sortWords(wordSortFunc: String => Int, words: List[String]): List[String] = {
+    words.sortBy(wordSortFunc)
+  }
 
-  words.sortBy(score)
+  def sortIntWords(wordSortFunc: Int => Int, words: List[Int]): List[Int] = {
+    words.sortBy(wordSortFunc)
+  }
+
+  def wordSortByLength(word: String): Int = {
+    word.length
+  }
+
+  def wordSortByCountOfS(word: String): Int = {
+    word.count(_ == 's')
+  }
+
+  def wordSortByBigOrder(word: Int): Int = {
+    -word
+  }
+
+  def wordSortByCountOfSByBigOrder(word: String): Int = {
+    -word.count(_ == 's')
+  }
+
+  def main(args: Array[String]): Unit = {
+    // println("Hello, world!")
+    // println(add(1, 2))
+    // println(increment(1))
+    // println(getFirstCharacter("Hello"))
+    // println(wordScore("Hello"))
+    println(rankedWords(wordScoreFunc, List("rust", "java")))
+    println(rankedWords(wordScoreFunc, List("java", "rust")))
+    println("----------------")
+    println(sortWords(wordSortByLength, List("scala", "rust", "ada")))
+    println(sortWords(wordSortByCountOfS, List("rust", "ada")))
+    println(sortIntWords(wordSortByBigOrder, List(5, 1, 2, 4, 3)))
+    println(sortWords(wordSortByCountOfSByBigOrder, List("ada", "rust")))
+  }
+
+  // def score(word: String): Int = {
+  //   word.replaceAll("a", "").length
+  // }
+
+  // val words = List("rust", "java")
+
+  // words.sortBy(score)
 }
