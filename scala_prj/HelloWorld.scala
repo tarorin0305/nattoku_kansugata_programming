8db6b6984b10d23913ea32a99f5603a0e96e97d6
tarorin0305 <dayutakatarorin@gmail.com>
2023-10-15 11:06:22 +0900

    update

--- scala_prj/HelloWorld.scala
+++ scala_prj/HelloWorld.scala
@@ -15,6 +15,16 @@ object Main {
     }
   }
 
+  def getTipPercentage(names: List[String]): Int = {
+    if (names.length > 5) {
+      20
+    } else if (names.length > 0) {
+      10
+    } else {
+      0
+    }
+  }
+
   def add(a: Int, b: Int): Int = {
     return a + b
   }
