b013912208d782ee0d3fa92e1a3d7afccd141fbb
rintaro.yamazaki <dayutakatarorin@gmail.com>
2023-10-28 12:46:49 +0900

    use func as flatMap func

--- scala_prj/Pipeline.scala
+++ scala_prj/Pipeline.scala
@@ -15,6 +15,14 @@ object Pipeline {
   val ys = List(-2, 7)
   val zs = List(3, 4)
 
+  def isInside(point: Point, radius: Int): Boolean = {
+    radius * radius >= point.x * point.x + point.y * point.y
+  }
+
+  def vaildRadiusFilter(radius: Int): List[Int] = {
+    if (radius >= 0) List(radius) else List.empty
+  }
+
   def bookAdaptions(author: String): List[Movie] = {
     if (author == "Tolkien") {
       List(Movie("An Unexpected Journey"), Movie("The Desolation of Smaug"))
@@ -40,36 +48,42 @@ object Pipeline {
   def recommendationFeed(books: List[Book]) = ???
 
   def main(args: Array[String]): Unit = {
-    val result = books.flatMap(book =>
-      book.authors.flatMap(author =>
-        bookAdaptions(author).map(movie =>
-          s"You may like ${movie.title}, " +
-          s"because you lied $author's ${book.title}"
-        )
-      )
-    )
+    val points = List(Point(1, 1), Point(5, 2), Point(3, 3))
+    val radiuses = List(2, 1)
+    val riskyRadiuses = List(-10, 0, 2)
+    val result = for {
+      r <- radiuses
+      point <- points.filter(p => isInside(p, r))
+    } yield s"$point is inside within a radius of $r"
     println(result)
 
     val result2 = for {
-      book <- books
-      author <- book.authors
-      movie <- bookAdaptions(author)
-    } yield s"You may like ${movie.title}, " +
-            s"because you liked $author's ${book.title}"
+      r <- radiuses
+      point <- points
+      if isInside(point, r)
+    } yield s"$point is inside within a radius of $r"
     println(result2)
 
     val result3 = for {
-      x <- xs
-      y <- ys
-      z <- zs
-    } yield Point3d(x, y, z)
+      r <- riskyRadiuses.filter(radius => radius >= 0)
+      point <- points.filter(p => isInside(p, r))
+    } yield s"$point is inside within a radius of $r"
+
+    val result4 = for {
+      r <- riskyRadiuses
+      if r >= 0
+      point <- points.filter(p => isInside(p, r))
+    } yield s"$point is inside within a radius of $r"
+
+    val result5 = for {
+      radius <- riskyRadiuses
+      // flatMapに渡せる関数を使用している。 つまり集合の要素を受け取り、集合(空集合を含む)を返す関数
+      validRadius <- vaildRadiusFilter(radius)
+      point <- points.filter(p => isInside(p, validRadius))
+    } yield s"$point is inside within a radius of $validRadius"
+    // println val result3 to 5
     println(result3)
-
-    val result4 = xs.flatMap( x =>
-      ys.flatMap( y =>
-        zs.map(z => Point3d(x, y, z)))
-    )
     println(result4)
-
+    println(result5)
   }
 }
