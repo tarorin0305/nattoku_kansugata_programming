09172b031c1007fb68412c6cb6802d5ec414a672
tarorin0305 <dayutakatarorin@gmail.com>
2023-11-03 23:41:30 +0900

    use Either

--- scala_prj/ErrorHandling.scala
+++ scala_prj/ErrorHandling.scala
@@ -12,23 +12,19 @@ object ErrorHandling {
   def extractName(rawShow: String): Option[String] = {
     val bracketOpen = rawShow.indexOf('(')
     if (bracketOpen > 0)
-      Some(rawShow.substring(0, bracketOpen).trim)
-    else None
+      Right(rawShow.substring(0, bracketOpen).trim)
+    else Left(s"Could not extract name from $rawShow")
   }
 
   def extractYearStart(rawShow: String): Either[String, Int] = {
     val bracketOpen = rawShow.indexOf('(')
     val dash = rawShow.indexOf('-')
-    val yearStrEither = if (bracketOpen != -1 && dash > bracketOpen + 1)
-                          Right(rawShow.substring(bracketOpen + 1, dash))
-                        else Left(s"Could not extract year start from $rawShow")
-    yearStrEither.map(yearStr => yearStr.toIntOption.toRight(s"Could not parse $yearStr")).flatten
-    // for {
-    //   yearStr <- if (bracketOpen != -1 && dash > bracketOpen + 1)
-    //                 Some(rawShow.substring(bracketOpen + 1, dash))
-    //               else None
-    //   year <- yearStr.toIntOption
-    // } yield year
+    for {
+      yearStartStr <- if (bracketOpen != -1 && dash > bracketOpen + 1)
+                        Right(rawShow.substring(bracketOpen + 1, dash))
+                      else Left(s"Could not extract year start from $rawShow")
+      yearStart <- yearStartStr.toIntOption.toRight(s"Could not parse $yearStartStr")
+    } yield yearStart
   }
 
   def extractYearEnd(rawShow: String): Option[Int] = {
@@ -36,9 +32,9 @@ object ErrorHandling {
     val dash = rawShow.indexOf('-')
     for {
       yearEndStr <- if (bracketClose != -1 && bracketClose > dash + 1)
-                      Some(rawShow.substring(dash + 1, bracketClose))
-                    else None
-      yearEnd <- yearEndStr.toIntOption
+                      Right(rawShow.substring(dash + 1, bracketClose))
+                    else Left(s"Could not extract year end from $rawShow")
+      yearEnd <- yearEndStr.toIntOption.toRight(s"Could not parse $yearEndStr")
     } yield yearEnd
   }
 
@@ -48,9 +44,10 @@ object ErrorHandling {
     val bracketClose = rawShow.indexOf(')')
     for {
       singleYearStr <- if (dash == -1 && bracketOpen > 0 && bracketClose > 0) {
-                          Some(rawShow.substring(bracketOpen + 1, bracketClose))
-                        } else None
-      singleYear <- singleYearStr.toIntOption
+                          Right(rawShow.substring(bracketOpen + 1, bracketClose))
+                        }
+                        else Left(s"Could not extract single year from $rawShow")
+      singleYear <- singleYearStr.toIntOption.toRight(s"Could not parse $singleYearStr as Int")
     } yield singleYear
   }
 
