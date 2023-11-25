0627603d94680ff2ec1ac7a15371db3c19c244eb
tarorin0305 <dayutakatarorin@gmail.com>
2023-11-25 23:19:27 +0900

    とりあえず動くように

--- scala_prj/InputOutput.scala
+++ scala_prj/InputOutput.scala
@@ -1,5 +1,4 @@
 import java.util.Random
-// import IO from cats.effect.IO
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 
@@ -7,7 +6,15 @@ object InputOutput {
   case class MeetingTime(startHour: Int, endHour: Int)
 
   def calendarEntriesApiCall(name: String): List[MeetingTime] = {
-    ???
+    val rand = new Random()
+    if (rand.nextFloat() < 0.25) {
+      throw new RuntimeException("API call failed")
+    }
+    name match {
+      case "Alice" => List(MeetingTime(8, 10), MeetingTime(11, 12))
+      case "Bob" => List(MeetingTime(9, 10))
+      case _ => List(MeetingTime(rand.nextInt(5) + 8, rand.nextInt(4) + 13))
+    }
   }
 
   def calendarEntries(name: String): IO[List[MeetingTime]] = {
@@ -15,14 +22,11 @@ object InputOutput {
   }
 
   def createMeetingApiCall(names: List[String], meetingTime: MeetingTime): Unit = {
-    ???
-    // static void createMeetingApiCall(List<String> names, MeetingTime meetingTime) {
-    //   Random random = new Random();
-    //   if (random.nextFloat() < 0.25) {
-    //     throw new RuntimeException("Failed to create meeting");
-    //   }
-    //   System.out.printf("SIDE-EFFECT")
-    // }
+    val random = new Random()
+    if (random.nextFloat() < 0.25) {
+      throw new RuntimeException("Failed to create meeting")
+    }
+    println("SIDE-EFFECT")
   }
 
   def createMeeting(names: List[String], meeting: MeetingTime): IO[Unit] = {
@@ -37,6 +41,7 @@ object InputOutput {
   }
 
   def main(args: Array[String]): Unit = {
-
+    var scheduledMeetingProgram = scheduleMeeting("Alice", "Bob")
+    println(scheduledMeetingProgram.unsafeRunSync())
   }
 }
