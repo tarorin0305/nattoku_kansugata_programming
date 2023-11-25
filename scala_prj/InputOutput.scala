eb3494828b7a739272a2cf66f9cd55b873a14870
tarorin0305 <dayutakatarorin@gmail.com>
2023-11-25 18:57:09 +0900

    import cats.effect
    libraryDependencies += "org.typelevel" %% "cats-effect" % "3.2.9"

--- scala_prj/InputOutput.scala
+++ scala_prj/InputOutput.scala
@@ -1,9 +1,16 @@
 import java.util.Random
+// import IO from cats.effect.IO
+import cats.effect.IO
+
 object InputOutput {
   case class MeetingTime(startHour: Int, endHour: Int)
 
   def calendarEntriesApiCall(name: String): List[MeetingTime] = {
     ???
+  }
+
+  def calendarEntries(name: String): IO[List[MeetingTime]] = {
+    IO.delay(calendarEntriesApiCall(name))
   }
 
   def createMeetingApiCall(names: List[String], meetingTime: MeetingTime): Unit = {
--- scala_prj/build.sbt
+++ scala_prj/build.sbt
@@ -1,1 +1,2 @@
 scalaVersion := "3.3.1"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "3.2.9"
